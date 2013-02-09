78e6e68aecd3460ff96a47f44094bfe3ea96c67d
Ben Evans <ben@bensbit.co.uk>
2013-02-09 01:23:13 +0000

    sperate db and structure

--- app.js
+++ app.js
@@ -1,77 +1,38 @@
-var express =require('express');
-var app = express();
-var request = require('request');
-var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
-var cachey = require('cachey')({redisClient:redis});
-var RegClient = require('npm-registry-client');
+
+/**
+ * Dependencies
+ */
+
+var express =require('express'),
+    app = express(),
+    request = require('request'),
+    redis = require('redis-url').connect(process.env.REDISTOGO_URL),
+    cachey = require('cachey')({redisClient:redis}),
+    db = new (require('./lib/db'))(cachey, 60 * 60);
+
 require('datejs');
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
-
-var db = {};
-db.getPackage = function(package, cb) {
-  cachey.cache('package:' + package, 60 * 60, function(cb) {
-    request({
-      uri: 'https://isaacs.iriscouch.com/downloads/_design/app/_view/pkg?group_level=2&start_key=%5B%22' + package + '%22%5D&end_key=%5B%22' + package + '%22,%7B%7D%5D',
-      json: true
-    }, function(err, res, body) {
-
-      var data = {};
-
-      for(var i in body.rows) {
-        data[body.rows[i].key[1]] = body.rows[i].value;
-      }
-      return cb(err, JSON.stringify(data));
-    });
-  }, function(err, body) {
-    if(err) return cb(err);
-    body = JSON.parse(body);
-    cb(null, body);
-  });
-};
-
-db.get30Days = function(package, cb) {
-  db.getPackage(package, function(err, data) {
-    if(err) return cb(err);
-    var days = [];
-    var equipDate = Date.parse('-1months');
-    for(i=30;i>0;i--) {
-      var dateStr = equipDate.toString("yyyy-MM-dd");
-      days.push({date:dateStr, downloads:data[dateStr] || 0});
-      equipDate = equipDate.add(1).days();
-    }
-    cb(null, days);
-  });
-};
-
-db.get7Days = function(package, cb) {
-  db.getPackage(package, function(err, data) {
-    if(err) return cb(err);
-    var days = [];
-    var equipDate = Date.parse('-7days');
-    for(i=7;i>0;i--) {
-      var dateStr = equipDate.toString("yyyy-MM-dd");
-      days.push({date:dateStr, downloads:data[dateStr] || 0});
-      equipDate = equipDate.add(1).days();
-    }
-    cb(null, days);
-  });
-};
-
-db.getPackagesByUser = function(name, cb) {
-  var regClient = new RegClient({registry:'http://registry.npmjs.org/', cache:'./'});
-  regClient.get('/-/_view/browseAuthors' + name, 0, cb);
-};
+/**
+ * Configure App
+ */
 
 app.configure(function() {
   app.set('view engine', 'html');
   app.engine('html', require('hbs').__express);
   app.use(express['static']('./public'));
 });
+
+if(process.env.CROSS_SITE) {
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+}
+
+/**
+ * Routes
+ */
 
 app.get('/', function(req, res) {
   res.redirect('/package/npm');
